b96af145baff5adcfbefde5623acee6fbed2992a
drusk1 <drusk1@OVDIMC10-207V.coast.cccd.adx>
2019-09-04 12:37:46 -0700

    nothing new

--- my-new-test-project/src/MyNewTestProgram.java
+++ my-new-test-project/src/MyNewTestProgram.java
@@ -5,7 +5,7 @@ public class MyNewTestProgram
     public static void main(String[] args)
     {
         // TODO Auto-generated method stub
-        System.out.println("Hi");
+        System.out.println("Hello");
         System.out.println("There");
     }
 
